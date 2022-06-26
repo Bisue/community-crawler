6026d0140090d1399614d3a8ca0d2ae9acdc33b0
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-06-26 20:38:48 +0900

    Revert "for gcp"
    This reverts commit a92b091e924842765b751172164289f06ec518af.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import Dcinside from './request/dcinside';
-import fs from 'fs';
 
 (async () => {
   const dcinside = new Dcinside();
 
   console.log('Crawl start');
 
-  const posts = await dcinside.fetch('baseball_new11', 1, 100);
-  fs.writeFileSync('./output.json', JSON.stringify(posts, null, 2));
+  const posts = await dcinside.fetch('baseball_new11', 1, 10);
+  console.log(posts, posts.length);
 
   console.log('Crawl finished!');
-  console.log(posts, posts.length);
 })();
